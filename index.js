8a6eaa7639267d16cdbc021c2969d864ce520cf0
TanvirAlamSk <ta89725@gmail.com>
2024-06-24 15:06:12 +0600

    update something

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const port = 5000
 app.use(cors())
 app.use(express.json())
 
-const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASSWORD}@cluster0.7xhaxuz.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://tanvir_alam:WZ7IuzBQUBmdPF3C@cluster0.7xhaxuz.mongodb.net/?retryWrites=true&w=majority`;
+// const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASSWORD}@cluster0.7xhaxuz.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
     serverApi: {
@@ -22,7 +23,8 @@ const client = new MongoClient(uri, {
 
 
 const createToken = (email) => {
-    const token = jwt.sign({ email }, process.env.SECRET_KEY, { expiresIn: "2 days" })
+    const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: "2 days" }
+    )
     return token;
 }
 
@@ -30,8 +32,8 @@ const verifyToken = (req, res, next) => {
     if (!req.headers.authorization) {
         return res.status(401).send("Unauthorized Access")
     }
-    const authorization = req.headers.authorization.split(" ")[1]
-    jwt.verify(authorization, process.env.SECRET_KEY, function (error, decoded) {
+    const token = req.headers.authorization.split(" ")[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (error, decoded) {
         if (error) {
             return res.status(403).send("Forbidden Access")
         }
@@ -45,7 +47,9 @@ async function run() {
         // await client.connect();
         const coursesCollection = client.db("talentlms").collection("courses")
         const usersCollection = client.db("talentlms").collection("users")
+        const enrollCourseCollection = client.db("talentlms").collection("enrollCourse")
 
+        // payment
         app.post("/create-payment-intent", async (req, res) => {
             const price = req.body.price
             const amount = price * 100
@@ -59,14 +63,14 @@ async function run() {
             res.send({ clientSecret: paymentIntent.client_secret })
         })
 
-
         app.get("/courses", async (req, res) => {
             const query = {}
             const result = await coursesCollection.find(query).toArray()
             res.send(result)
         })
 
-        app.post("/courses", verifyToken, async (req, res) => {
+        app.post("/courses", async (req, res) => {
+            // verifyToken
             const data = req.body
             const result = await coursesCollection.insertOne(data)
             res.send(result)
@@ -79,9 +83,10 @@ async function run() {
             const result = await coursesCollection.findOne(query)
             res.send(result)
         })
+
         app.get("/my-courses", async (req, res) => {
             const email = req.query.email
-            const query = { email }
+            const query = { owner_email: email }
             const result = await coursesCollection.find(query).toArray()
             res.send(result)
         })
@@ -106,11 +111,11 @@ async function run() {
 
 
         // only login user
-        app.get("/usersCollection", async (req, res) => {
+        app.get("/users", verifyToken, async (req, res) => {
             const email = req.query.email
-            // if (email !== req.decoded.email) {
-            //     return res.status(403).send("Forbidden Access")
-            // }
+            if (email !== req.decoded.email) {
+                return res.status(403).send("Forbidden Access")
+            }
             // verifyToken,
             const query = { email: email }
             const result = await usersCollection.findOne(query)
@@ -120,28 +125,51 @@ async function run() {
         app.post("/users", async (req, res) => {
             const data = req.body
             const query = { email: data.email }
+            // if (req.body.email !== req.decoded.email) {
+            //     return res.status(403).send("Forbidden Access")
+            // }
             const isExists = await usersCollection.findOne(query)
             const token = createToken(data.email)
-
             if (!isExists) {
                 const result = await usersCollection.insertOne(data)
                 return res.send({ result, token })
             } else {
                 return res.send({ token })
             }
+        }
+        )
 
+        app.patch("/users", async (req, res) => {
+            const filter = { email: req.query.email }
+            const updatedDoc = {
+                $set: req.body
+            }
+            const options = { upsert: true }
+            const result = await users.updateOne(filter, updatedDoc, options)
+            res.send(result)
         })
 
-        // app.patch("/users", async (req, res) => {
-        //     const filter = { email: req.query.email }
-        //     const updatedDoc = {
-        //         $set: req.body
-        //     }
-        //     const options = { upsert: true }
-        //     const result = await users.updateOne(filter, updatedDoc, options)
-        //     console.log(result)
-        //     res.send(result)
-        // })
+        // enroll Course
+        // all
+        app.get(("/enroll-course"), async (req, res) => {
+            const email = req.query.email
+            const query = { student_email: email }
+            const result = await enrollCourseCollection.find(query).toArray()
+            res.send(result)
+        })
+        // one
+        app.get(("/enroll-course/:id"), async (req, res) => {
+            const id = req.params.id
+            const query = { course_id: id }
+            const result = await enrollCourseCollection.findOne(query)
+            res.send(result)
+        })
+        app.post("/enroll-course", async (req, res) => {
+            const data = req.body
+            const result = await enrollCourseCollection.insertOne(data)
+            res.send(result)
+        })
+
 
     } finally {
 
